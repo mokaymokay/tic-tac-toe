f6bc7c386441fa4bf41938d4c26f2b4672d964e5
Kay Mok <kaymok3@gmail.com>
2018-02-09 16:40:14 -0500

    Finish implementation to display and jump to previous moves by updating the state using jumpTo

--- src/index.js
+++ src/index.js
@@ -47,15 +47,18 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [{
+      history: [
+        {
         squares: Array(9).fill(null)
-      }],
-      xIsNext: true
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -63,16 +66,26 @@ class Game extends React.Component {
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      history: history.concat([{
+      history: history.concat([
+        {
         squares: squares
-      }]),
-      xIsNext: !this.state.xIsNext,
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
